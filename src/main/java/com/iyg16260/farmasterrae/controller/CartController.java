27ad6d0314031ec8233cb39d5aff1e2fbd995c22
juanyaferox <juaniago2001@gmail.com>
2025-05-04 20:38:14 +0200

    Tiempo 1 hora. Se ha solucionado el caso ver carrito, a faltar de comprobar su funcionamiento.

--- src/main/java/com/iyg16260/farmasterrae/controller/CartController.java
+++ src/main/java/com/iyg16260/farmasterrae/controller/CartController.java
@@ -1,13 +1,14 @@
 package com.iyg16260.farmasterrae.controller;
 
-import com.iyg16260.farmasterrae.model.Product;
+import com.iyg16260.farmasterrae.dto.products.ProductDTO;
 import com.iyg16260.farmasterrae.service.CartService;
-import com.iyg16260.farmasterrae.service.ProductsService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping ("/cart")
@@ -15,34 +16,40 @@ public class CartController {
 
     @Autowired
     CartService cartService;
-    
+
     @GetMapping
-    public ModelAndView viewCart (HttpSession session) {
+    public ModelAndView viewCart(HttpSession session) {
+        Map<ProductDTO, Integer> cart = cartService.getDetailedProducts(session);
+        double totalAmount = 0.0;
+        for (Map.Entry<ProductDTO, Integer> entry : cart.entrySet()) {
+            totalAmount += entry.getKey().getPrice() * entry.getValue();
+        }
         return new ModelAndView("cart/cart-view")
-                .addObject("cart", cartService.getCart(session));
+                .addObject("products", cart)
+                .addObject("totalAmount", totalAmount);
     }
 
     @PostMapping ("/add/{reference}")
-    public String addToCart (@PathVariable String reference, HttpSession session) {
+    public String addToCart(@PathVariable String reference, HttpSession session) {
         cartService.addProductToCart(reference, session);
 
         return "redirect:/products/" + reference + "?added=true";
     }
 
     @PostMapping ("/delete/{reference}")
-    public String deleteFromCart (@PathVariable String reference,
+    public String deleteFromCart(@PathVariable String reference,
                                  @RequestParam (defaultValue = "false", required = false) boolean deleteAll,
                                  HttpSession session) {
         if (deleteAll)
             cartService.deleteAllSameProductFromCart(reference, session);
         else
-            cartService.deleteProductFromCart(reference,session);
+            cartService.deleteProductFromCart(reference, session);
 
         return "redirect:/cart";
     }
 
     @PostMapping ("/clear")
-    public String clearCart (HttpSession session) {
+    public String clearCart(HttpSession session) {
         cartService.clearCart(session);
         return "redirect:/cart";
     }
--- src/main/java/com/iyg16260/farmasterrae/service/CartService.java
+++ src/main/java/com/iyg16260/farmasterrae/service/CartService.java
@@ -1,12 +1,16 @@
 package com.iyg16260.farmasterrae.service;
 
 import com.iyg16260.farmasterrae.dto.products.ProductDTO;
+import com.iyg16260.farmasterrae.model.Product;
 import com.iyg16260.farmasterrae.utils.SessionCart;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class CartService {
@@ -16,7 +20,7 @@ public class CartService {
 
     public static final String CART_SESSION_KEY = "cart";
 
-    public SessionCart getCart (HttpSession session) {
+    public SessionCart getCart(HttpSession session) {
         SessionCart cartObject = (SessionCart) session.getAttribute(CART_SESSION_KEY);
         if (cartObject == null) {
             SessionCart cart = new SessionCart();
@@ -26,35 +30,62 @@ public class CartService {
         return cartObject;
     }
 
-    public void addProductToCart (String refProduct, HttpSession session) {
+    public void addProductToCart(String refProduct, HttpSession session) {
         SessionCart cart = getCart(session);
         cart.addProduct(refProduct);
     }
 
-    public void deleteProductFromCart (String refProduct, HttpSession session) {
+    public void deleteProductFromCart(String refProduct, HttpSession session) {
         SessionCart cart = getCart(session);
         cart.deleteOneProduct(refProduct);
     }
 
-    public void deleteAllSameProductFromCart (String refProduct, HttpSession session) {
+    public void deleteAllSameProductFromCart(String refProduct, HttpSession session) {
         SessionCart cart = getCart(session);
         cart.deleteAllSameProduct(refProduct);
     }
 
-    public void clearCart (HttpSession session) {
+    public void clearCart(HttpSession session) {
         SessionCart cart = getCart(session);
         cart.clear();
     }
 
-    public int getCartSize (HttpSession session) {
+    public int getCartSize(HttpSession session) {
         SessionCart cart = getCart(session);
         return cart.getSize();
     }
 
-    public List<ProductDTO> getDetailedProducts(HttpSession session) {
+    /**
+     * Map productDTO + cantidad en lugar de la referencia + cantidad
+     *
+     * @param session
+     * @return
+     */
+    public Map<ProductDTO, Integer> getDetailedProducts(HttpSession session) {
         SessionCart cart = getCart(session);
-        return cart.getProducts().keySet().stream()
-                .map(reference -> productsService.getProductDTOByReference(reference))
+        return cart.getProducts()
+                .entrySet().stream()
+                .collect(Collectors.toMap(reference ->
+                                productsService.getProductDTOByReference(reference.getKey()),
+                        Map.Entry::getValue// q -> q.getValue
+                ));
+    }
+
+    /**
+     * Listado de productos en el carrito, segun la cantidad de cada
+     *
+     * @param session
+     * @return
+     */
+    public List<Product> getProducts(HttpSession session) {
+        SessionCart cart = getCart(session);
+        return cart.getProducts()
+                .entrySet().stream()
+                .flatMap(cartProducts ->
+                        IntStream.range(0, cartProducts.getValue())
+                                .mapToObj(quantity ->
+                                        productsService.getProductByReference(cartProducts.getKey())))
                 .toList();
     }
+    
 }
