f75d863118d45ce95d40b896781edcbe4448760f
juanyaferox <juaniago2001@gmail.com>
2025-05-15 00:05:42 +0200

    Pequeños arreglos.

--- src/main/java/com/iyg16260/farmasterrae/dto/user/UserDTO.java
+++ src/main/java/com/iyg16260/farmasterrae/dto/user/UserDTO.java
@@ -2,11 +2,13 @@ package com.iyg16260.farmasterrae.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO {
     @NotBlank (message = "El usuario no puede estar vacío")
+    @Size (max = 50, message = "El título debe tener un máximo de 50 caracteres")
     String username;
     @NotBlank (message = "El correo no puede estar vacío")
     @Email (message = "Debe ser un correo")
